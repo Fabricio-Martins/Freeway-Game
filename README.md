234d1a2bce7ec7a7feb0859bfe2c89c7ba199d62
Fabricio Flávio <fabriciod@alunos.utfpr.edu.br>
2023-09-06 19:44:36 -0300

    Lógica do Pause consertada

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Clone do jogo Freeway
 Está faltando:
 
 - [ ]  De tempo em tempo um item é spawnado em uma posição da tela e ele some depois de um tempo. (Melhor, fazer por eventos, usando uma faixa no meio da tela avisando cada)
-    - [ ]  Slow nos adversários OU aumento da velocidade dos carros
-    - [ ]  Andar somente para frente e para trás (Eixo Y)
-    - [ ]  Névoa usando fade
-    - [ ]  Inverter as teclas
+	- [ ]  Slow nos adversários OU aumento da velocidade dos carros
+	- [ ]  Andar somente para frente e para trás (Eixo Y)
+	- [ ]  Névoa usando fade
+	- [ ]  Inverter as teclas
 - [ ]  Colocar um tutorial breve antes de iniciar o jogo (uma tela)
 
 - [ ]  Arrumar colisão do Player 1 no Multiplayer
--- Scenes/Scenario/Pause.gd
+++ Scenes/Scenario/Pause.gd
@@ -3,27 +3,34 @@ extends Control
 var pause = false
 
 func _ready():
+	pass
 	visible = false
 	#$ControlsHud.visible = false
 	
 func _input(event):
+	pass
 	if event.is_action_pressed("ui_cancel"):
 		pause =  !pause
 		get_tree().paused = pause
 		visible = pause
+		print("passou")
 
 func _on_ResumeButton_pressed():
-	pause = !pause
-	get_tree().paused = pause
-	visible = pause
+	print("passou1")
+	get_tree().paused = false
+	visible = false
+	print("passou2")
 
 func _on_ExitButton_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/Menu/menu.tscn")
 
 func _on_ControlsButton_pressed():
-	pass
-	#visible = false
+	$".".visible = false
 	#$ControlsHud.visible = true
+
+func _on_ReturnButton_pressed():
+	$".".visible = true
+	#$ControlsHud.visible = false
 
 func _on_menu_pressed():
 	pause = !pause
